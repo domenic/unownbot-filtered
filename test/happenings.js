3329dd0b9f4d8a7727c14407aae9c3bea9b90039
Domenic Denicola <d@domenic.me>
2016-09-05 16:21:39 -0400

    Finish up the tests for existing functionality

--- test/happenings.js
+++ test/happenings.js
@@ -20,6 +20,8 @@ test("emits a 'connected' event when the tweet stream becomes connected", t => {
 });
 
 test("emits an 'error' event when the tweet stream errors", t => {
+  t.plan(2);
+
   const fakeStream = new EventEmitter();
   const happenings = createHappenings(fakeStream, [], "12345");
   const fakeError = new Error("boo");
@@ -33,6 +35,8 @@ test("emits an 'error' event when the tweet stream errors", t => {
 });
 
 test("emits a 'desired spawn' event when something desired spawns", t => {
+  t.plan(2);
+
   const fakeStream = new EventEmitter();
   const happenings = createHappenings(fakeStream, ["Dragonite"], "754728450573930496");
 
@@ -50,4 +54,42 @@ test("emits a 'desired spawn' event when something desired spawns", t => {
   });
 
   fakeStream.emit("tweet", fixtureTweets[0]);
+});
+
+test("emits an 'undesired spawn' event when something undesired spawns", t => {
+  t.plan(2);
+
+  const fakeStream = new EventEmitter();
+  const happenings = createHappenings(fakeStream, ["Gengar"], "754728450573930496");
+
+  happenings.on("undesired spawn", (...args) => {
+    t.pass("'undesired spawn' event must be emitted");
+    t.deepEqual(
+      args,
+      [{
+        pokemon: "Dragonite",
+        location: "380 Riverside Dr, 10025",
+        until: "3:56:05 PM",
+        url: "https://goo.gl/LhphUu"
+      }]
+    );
+  });
+
+  fakeStream.emit("tweet", fixtureTweets[0]);
+});
+
+test("does not emit an event if the tweet is from a different user", t => {
+  const fakeStream = new EventEmitter();
+  const happenings = createHappenings(fakeStream, ["Dragonite"], "1234");
+
+  happenings.on("desired spawn", () => {
+    t.fail("'desired spawn' event must not be emitted");
+  });
+  happenings.on("undesired spawn", () => {
+    t.fail("'undesired spawn' event must not be emitted");
+  });
+
+  const fakeTweet = JSON.parse(JSON.stringify(fixtureTweets[0]));
+  fakeTweet.user.id_str = "1235";
+  fakeStream.emit("tweet", fakeTweet);
 });
