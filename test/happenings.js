807b95b14d1b237153f91de81910d4d1c98b271c
Domenic Denicola <d@domenic.me>
2016-09-05 16:03:20 -0400

    Add testing infrastructure and a basic test
    Includes factoring out the happenings emitter into its own file.

---
+++ test/happenings.js
@@ -0,0 +1,19 @@
+"use strict";
+const { EventEmitter } = require("events");
+const test = require("ava");
+const createHappenings = require("../lib/happenings.js");
+
+test("emits connected event when the tweet stream becomes connected", t => {
+  t.plan(2);
+
+  const fakeStream = new EventEmitter();
+  const happenings = createHappenings(fakeStream);
+
+  happenings.on("connected", (...args) => {
+    t.pass("connected event must be emitted");
+    t.deepEqual(args, [], "zero arguments must be passed");
+  });
+
+  const fakeResponse = { fake: "response" };
+  fakeStream.emit("connected", fakeResponse);
+});
